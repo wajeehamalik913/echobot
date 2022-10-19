95aef4e6e1ca8ee7833f3cd1f3f65503f96a3f31
Wajeeha Malik <wajeehamalik913@gmail.com>
2022-10-19 12:21:18 +0400

    echo bot for teams

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var teamConversationData = turnContext.Activity.GetChannelData<TeamsChannelData>();
+            await turnContext.SendActivityAsync(MessageFactory.Text("Your ID is " + turnContext.Activity.From.Id), cancellationToken);
+            await turnContext.SendActivityAsync(MessageFactory.Text("My ID is " + turnContext.Activity.Recipient.Id), cancellationToken);
+            await turnContext.SendActivityAsync(MessageFactory.Text("Tenant ID is " + teamConversationData.Tenant.Id));
+            await turnContext.SendActivityAsync(MessageFactory.Text("Service URL is " + turnContext.Activity.ServiceUrl));
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Program
+    {
+        [Obsolete]
+        async static Task Main(string[] args)
+        {
+            //CreateHostBuilder(args).Build().Run();
+            const string url = " https://smba.trafficmanager.net/emea/";
+            const string appId = "10f2c6ee-b4a8-4bc4-a871-c39f7c652577";
+            const string appPassword = "qJI8Q~gU.~~A5fb~NQECptgWAU5hQhYCI0aLRaDW";
+            MicrosoftAppCredentials.TrustServiceUrl(url);
+            var client = new ConnectorClient(new Uri(url), appId, appPassword);
+            // Create or get existing chat conversation with user
+            var parameters = new ConversationParameters
+            {
+                Bot = new ChannelAccount("28:10f2c6ee-b4a8-4bc4-a871-c39f7c652577"),
+                Members = new[] { new ChannelAccount("29:1VCZRJNwIlly9hfb5dS3wjigxETakM7j7eh-EcZhoaTMPxeGD5T1YLEU_hBmvZUZF4bEw_3a8gjWmDGWIPsf82w") },
+                ChannelData = new TeamsChannelData
+                {
+                    Tenant = new TenantInfo("603439c3-58ad-4a91-8ed3-b53e9a8677b3"),
+                },
+            };
+            var response = await client.Conversations.CreateConversationAsync(parameters);
+            // Construct the message to post to conversation
+            var newActivity = new Activity
+            {
+                Text = "Hello",
+                Type = ActivityTypes.Message,
+                Conversation = new ConversationAccount
+                {
+                    Id = response.Id
+                },
+            };
+            // Post the message to chat conversation with user
+            await client.Conversations.SendToConversationAsync(response.Id, newActivity);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureLogging((logging) =>
+                    {
+                        logging.AddDebug();
+                        logging.AddConsole();
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
